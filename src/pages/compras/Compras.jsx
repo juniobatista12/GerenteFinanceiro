6da200e58ec4151382a98251f3091b2ee71ada92
juniobatista12 <junio.batista12@gmail.com>
2023-12-24 00:40:36 -0300

    Montando a lista com as compras adicionadas, testar persistência

--- src/pages/compras/Compras.jsx
+++ src/pages/compras/Compras.jsx
@@ -1,8 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonButton, IonFooter, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonButton, IonFooter, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
 import ExploreContainer from '../../components/ExploreContainer';
 import './Compras.css';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getFromStorage } from '../../components/Storage';
 import AddCompras from './AddCompras';
 
@@ -12,8 +12,12 @@ const Compras = () => {
 
   useEffect(() => {
     const getCompras = async () => {
-      const data = await getFromStorage('compras');
-      setCompras(data);
+      await getFromStorage('compras').then((data) => {
+        if(data.size()){
+          setCompras(data);
+        }
+      }
+      )
     }
 
     getCompras();
@@ -24,8 +28,10 @@ const Compras = () => {
   }
 
   const handleSubmit = (descricao, valor) => {
-    console.log(descricao)
-    console.log(valor)
+    const tmp = compras;
+    tmp.push({'descricao': descricao, 'valor': valor});
+    setCompras(tmp);
+    setShowAddForm(false);
   }
   
   return (
@@ -42,15 +48,20 @@ const Compras = () => {
           </IonToolbar>
         </IonHeader>
         <IonList>
-          <IonItem>
-            <IonLabel>Alubagre</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Bagrenícius</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Ivankov</IonLabel>
-          </IonItem>
+          {compras.map((compra, index) => {
+            console.log(compra)
+            return (
+              <IonItem>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol><IonLabel>{index+1}</IonLabel></IonCol>
+                    <IonCol size='9'><IonLabel>{compra.descricao}</IonLabel></IonCol>
+                    <IonCol><IonLabel>{compra.valor}</IonLabel></IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonItem>
+            )
+          })}
         </IonList>
         {showAddForm ? <AddCompras hide={hideAddForm} submit={handleSubmit} /> : null}
         <IonButton className='addButton' shape='round' onClick={() => setShowAddForm(true)}>
